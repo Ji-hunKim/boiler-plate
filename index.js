8594eec4d2bf87456491d2c3dc6843a05b02a5f7
Jihun-Kim <anam4561@naver.com>
2023-05-30 20:55:47 +0900

    add config & add register api

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const config = require('./config/key');
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://okol4561:okpo0513@first.2owuq.mongodb.net/?retryWrites=true&w=majority', {}).then(() => console.log('MongoDB Connected...')).catch(err => console.log(err))
+mongoose.connect(config.mongoURI, {}).then(() => console.log('MongoDB Connected...')).catch(err => console.log(err))
 
 const  { User } = require("./models/User");
 const bodyParser = require('body-parser');
@@ -15,12 +17,20 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.post('/register', (req,res) => {
+app.post('/register',(req,res)=>{   
+  //회원가입할 때 필요한 정보들을 client에서 가져오면,
+  //그 정보들을 DB에 넣어준다.
   const user = new User(req.body);
-  user.save((err, userInfo)=>{
-    if(err) return res.json({ success: false, err})
-    else res.status(200).json({ success: true })
-  })
+  //user모델에 정보가 저장됨
+  //실패 시, 실패한 정보를 보내줌
+  user.save().then(()=>{
+      res.status(200).json({
+          success:true
+      })
+  }).catch((err)=>{
+      return res.json({success:false,err})
+  });
+
 })
 
 app.listen(port, () => {
