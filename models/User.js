1dded9f5a3ccbdcece9ae61458189e30651f9a34
Jihun-Kim <anam4561@naver.com>
2023-05-30 21:32:30 +0900

    add bcrypt and pre save method

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds =  10;
 
 const userSchema = mongoose.Schema({
     name: {
@@ -29,6 +31,28 @@ const userSchema = mongoose.Schema({
     tokenExp: {
         type: Number
     }
+})
+
+userSchema.pre('save', function(next){
+    var user = this;
+    if(user.isModified('password')){
+// 비밀번호 암호화
+    // salt 생성
+    bcrypt.genSalt(saltRounds, function(err,salt){
+        if(err) return next(err)
+        
+        //비밀번호 생성됐다면
+        bcrypt.hash(user.password, salt, function(err,hash){
+            if(err) return next(err)
+
+            //암호화 성공되면 hash된 비밀번호로 변경
+            user.password = hash
+            next()
+        })
+    })
+    }
+    
+
 })
 
 const User = mongoose.model('User', userSchema)
