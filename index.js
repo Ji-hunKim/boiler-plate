bcc51c313e64b2f477f9e8fcf8e2a501bb979e21
Jihun-Kim <anam4561@naver.com>
2023-06-07 23:46:06 +0900

    add logout

--- index.js
+++ index.js
@@ -126,6 +126,16 @@ app.get("/api/users/auth", auth, (req, res) => {
   });
 });
 
+app.get("/api/users/logout", auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "" })
+    .then((user) => {
+      return res.status(200).send({ success: true });
+    })
+    .catch((err) => {
+      return res.json({ success: false, err });
+    });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
