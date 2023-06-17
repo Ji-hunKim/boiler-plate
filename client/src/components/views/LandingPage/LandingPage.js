e79c111aec28dd66dc9e4958191ffd2e77e4825a
Jihun-Kim <anam4561@naver.com>
2023-06-17 23:36:34 +0900

    logout added

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,11 +1,25 @@
 import React, { useEffect } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function LandingPage() {
   // api 서버로 보내고 (index.js)
+  const navigate = useNavigate();
+
   useEffect(() => {
     axios.get("/api/hello").then((response) => console.log(response.date));
   }, []);
+
+  const onClickHandler = () => {
+    axios.get("/api/users/logout").then((response) => {
+      if (response.data.success) {
+        navigate("/login");
+      } else {
+        alert("failed logout");
+      }
+    });
+  };
+
   return (
     <div
       style={{
@@ -17,6 +31,7 @@ function LandingPage() {
       }}
     >
       <h2>시작페이지</h2>
+      <button onClick={onClickHandler}>로그아웃</button>
     </div>
   );
 }
